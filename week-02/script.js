2b5ee53c6e6c6871337f4230a3501e23ef00ff4a
Matthew Carrillo <matthewcarr1115@gmail.com>
2026-01-29 11:50:36 -0500

    This is my week 2 code X3

---
+++ week-02/script.js
@@ -0,0 +1,24 @@
+var week = 2;
+
+console.log("Hello, this is week " + week + ".");
+
+// alert("Hello, this is really week 2!");
+
+var today = "thursday";
+// console.log(today);
+var tomorrow = "friday";
+
+// console.log("today is Thursday, January 29, 2025");
+// console.log("tomorrow is Friday, January 30, 2025");
+
+var greeting = "today is " + today + ".";
+
+console.log(greeting);
+
+var nextWeek = week + 1;
+console.log ("next week will be week " + nextWeek + ".");
+
+// var myName = "Matthew";
+
+var myName = prompt("What is your name?");
+alert("My name is " + myName);
